e099d1f70171c0a7388e21c2d5b98f57942b45c1
naja moe <najamoe@outlook.dk>
2025-09-23 16:14:37 +0200

    added instructor model

---
+++ StudentManagment/Models/Instructor.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentManagment.Models
+{
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public DateTime HireDate { get; set; }
+
+        // Navigation property for related courses
+        public ICollection<Courses> Courses { get; set; } = new List<Courses>();
+    }
+}
