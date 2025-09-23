0310a295b01e184e6dcd509d62e6ca535ae78623
naja moe <najamoe@outlook.dk>
2025-09-23 15:43:16 +0200

    middlename migration

---
+++ StudentManagment/Migrations/20250923133903_AddMiddleNameToStudent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagment.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMiddleNameToStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "Students");
+        }
+    }
+}
