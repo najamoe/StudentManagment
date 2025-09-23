00e534dd358cb82bd5c9f18455cc0946197c4bcb
naja moe <najamoe@outlook.dk>
2025-09-23 17:05:08 +0200

    added the departmenthead

--- StudentManagment/Models/Departments.cs
+++ StudentManagment/Models/Departments.cs
@@ -1,6 +1,18 @@
-﻿namespace StudentManagment.Models
+﻿using Microsoft.Identity.Client;
+
+namespace StudentManagment.Models
 {
     public class Departments
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int budget { get; set; }
+        public DateOnly StartDate { get; set; }
+
+        // Foreign key to instructor who is the department head
+        public int? DepartmentHead { get; set; }
+
+        //Navigation property
+        public Instructor? Instructor { get; set; }
     }
 }
