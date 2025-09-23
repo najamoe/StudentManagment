86bf13a4aefcf0b4a48c3bc8bdfffe9414e73ee6
naja moe <najamoe@outlook.dk>
2025-09-23 17:11:20 +0200

    Minor Changes to model

--- StudentManagment/Models/Departments.cs
+++ StudentManagment/Models/Departments.cs
@@ -4,15 +4,15 @@ namespace StudentManagment.Models
 {
     public class Departments
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+        public required string Name { get; set; }
         public int budget { get; set; }
         public DateOnly StartDate { get; set; }
 
         // Foreign key to instructor who is the department head
-        public int? DepartmentHead { get; set; }
+        public int? DepartmentHeadId { get; set; }
 
         //Navigation property
-        public Instructor? Instructor { get; set; }
+        public Instructor? DepartmentHead { get; set; }
     }
 }
