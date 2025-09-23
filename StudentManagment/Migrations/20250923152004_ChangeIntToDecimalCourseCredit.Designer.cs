874e1c5cb89a8e27b297ae424220d30810b111b7
naja moe <najamoe@outlook.dk>
2025-09-23 17:20:41 +0200

    migrated change to course credit

--- StudentManagment/Migrations/20250923152004_ChangeIntToDecimalCourseCredit.Designer.cs
+++ StudentManagment/Migrations/20250923152004_ChangeIntToDecimalCourseCredit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentManagment.Data;
 
@@ -11,9 +12,11 @@ using StudentManagment.Data;
 namespace StudentManagment.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250923152004_ChangeIntToDecimalCourseCredit")]
+    partial class ChangeIntToDecimalCourseCredit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
